829239d9f9f00c973a86424909da215ae00e7553
kodjo <maformation52@gmail.com>
2017-09-06 14:32:36 +0200

    Procedure de modification des posts

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -5,9 +5,11 @@ namespace AppBundle\Controller;
 
 use AppBundle\AppBundle;
 use AppBundle\Entity\Post;
+use AppBundle\Form\PostType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -58,5 +60,41 @@ class PostController extends Controller
             "title" => "Liste des posts par année ({$year})",
             "postList" => $postrepository->getPostByYear($year)
         ]);
+    }
+
+    /**
+     * @route("/post/modif/{id}", name="post_edit")
+     * @param Request $request
+     * @param Post $post
+     * @return Response
+     */
+    public function editAction(Request $request, Post $post){
+
+        $user = $this->getUser();
+        $role = isset($user)?$user->getRoles():[];
+        $userId = isset($user)?$user->getId(): null;
+        if ((!in_array("ROLE_AUTHOR", $role) || $userId != $post->getAuthor()->getId() )){
+            throw new  AccessDeniedException("vous n'avez pas les droits pour modifier ce post");
+        }
+
+        //creation du formulaire
+        $form = $this->createForm(PostType:: class, $post);
+
+        //hydratation de l'entité
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() and $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em ->persist($post);
+            $em->flush();
+
+            //redirection
+            return $this->redirectToRoute(
+                "theme_details",
+                ["id" =>$post->getTheme()->getId()]
+            );
+        }
+
+        return $this->render("post/edit.html.twig", ["postForm" =>$form->createView()]);
     }
 }
